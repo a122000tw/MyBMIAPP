8307baa2c6f5c10d837cc88893f240f04c94d34a
MB-study <a122000tw@gmail.com>
2021-04-10 14:06:49 +0800

    2021

--- app_luckyindex/src/main/java/com/applab/app_luckyindex/MainActivity.kt
+++ app_luckyindex/src/main/java/com/applab/app_luckyindex/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.applab.app_luckyindex
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+    lateinit var context: Context // 環境資訊
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+    }
+
+    fun onClick(view: View) {
+        Log.d(TAG, input_number.text.toString())
+        val intent = Intent(context,ResultActivity::class.java)
+        // 將input_number 所輸入的資料傳送到下一個 Activity (ResultActivity)
+        intent.putExtra("number", input_number.text.toString())
+        // 轉跳 Activity
+        openResultActivityCustom.launch(intent)
+    }
+
+    private val openResultActivityCustom =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == 101) {
+                AlertDialog.Builder(context)
+                    .setTitle(R.string.app_result_title)
+                    .setMessage(result.data?.getStringExtra("luckyIndex").toString())
+                    .setPositiveButton("OK",null)
+                    .show()
+                }
+
     }
 }
