ac0a9b7621ed64b14b0e7fd3bc891c05d284db44
MB-study <a122000tw@gmail.com>
2021-04-10 16:31:41 +0800

    2021

--- app_exchange/src/main/java/com/applab/app_exchange/Exchange.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/Exchange.kt
@@ -6,3 +6,8 @@ fun getUSDByTWD(amount: Int): Double {
     val rate = YahooFinance.get("TWDUSD=x").quote.price.toDouble()
     return amount * rate
 }
+
+fun getAmountByTWDAndSymbol (amount: Int, symbol: String): Double {
+    val rate = YahooFinance.get("TWD${symbol}=x").quote.price.toDouble()
+    return amount * rate
+}
--- app_exchange/src/main/java/com/applab/app_exchange/MainActivity.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         context = this
+
+        // 配置 spinner 的下拉選單資料
+        val symbols = arrayOf("USD", "JPY", "CNY", "AUD", "EUR")
+        // 適配器 adapter (管理資料與資料UI的配置)
+        // 正常樣貌
+        val adapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, symbols)
+        // 下拉後樣貌
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        spinner.adapter = adapter
+
+
     }
 
     fun onClick(view: View) {
 
         val intent = Intent(context, ExchangeActivity::class.java)
         intent.putExtra("amount", input_twd.text.toString().toInt())
+        intent.putExtra("symbol", spinner.selectedItem.toString())
         startActivity(intent)
     }
 
