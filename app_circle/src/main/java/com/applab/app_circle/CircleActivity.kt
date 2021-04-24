efb087c4285483367eedcba82369420b6e376375
MB-study <a122000tw@gmail.com>
2021-04-24 10:46:10 +0800

    score

--- app_circle/src/main/java/com/applab/app_circle/CircleActivity.kt
+++ app_circle/src/main/java/com/applab/app_circle/CircleActivity.kt
@@ -2,10 +2,25 @@ package com.applab.app_circle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_circle.*
+import kotlinx.android.synthetic.main.activity_circle.edit_r
+
 
 class CircleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_circle)
+
+        btn_submit.setOnClickListener {
+            val r = edit_r.text.toString().toInt()
+            intent.putExtra("r", r)
+            setResult(101, intent)
+            finish()
+        }
+
+
     }
+
+
+
 }
--- app_circle/src/main/java/com/applab/app_circle/EarthActivity.kt
+++ app_circle/src/main/java/com/applab/app_circle/EarthActivity.kt
@@ -2,10 +2,21 @@ package com.applab.app_circle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_earth.btn_submit
+import kotlinx.android.synthetic.main.activity_earth.edit_r
+
 
 class EarthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_earth)
+
+        btn_submit.setOnClickListener {
+            val r = edit_r.text.toString().toInt()
+            intent.putExtra("r", r)
+            setResult(201, intent)
+            finish()
+        }
+
     }
 }
