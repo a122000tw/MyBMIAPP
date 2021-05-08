7e9cbbe7ad9348889ab94376023cc7dcc48a41f8
MB-study <a122000tw@gmail.com>
2021-05-08 12:14:18 +0800

    score

--- app_nav1/src/main/java/com/applab/app_nav1/WelcomeFragment.kt
+++ app_nav1/src/main/java/com/applab/app_nav1/WelcomeFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragement_welcome.*
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class WelcomeFragment : Fragment(R.layout.fragement_welcome) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        button_confirm.setOnClickListener {
-            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+        button_welcome.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
             findNavController().navigate(action)
         }
+
     }
 }
