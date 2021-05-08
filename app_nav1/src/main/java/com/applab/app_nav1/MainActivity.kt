a1790e209e924fdd487fd779c310f6d5fd960895
MB-study <a122000tw@gmail.com>
2021-05-08 12:44:59 +0800

    score

--- app_nav1/src/main/java/com/applab/app_nav1/MainActivity.kt
+++ app_nav1/src/main/java/com/applab/app_nav1/MainActivity.kt
@@ -2,10 +2,35 @@ package com.applab.app_nav1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        // Bottom bar 切換時會變色
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment)
+        )
+
+        // 手動加入 ActionBar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
