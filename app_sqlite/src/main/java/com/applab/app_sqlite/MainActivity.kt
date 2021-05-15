63bf9946439dbc4be1b27eba2188a12ba12dcdec
MB-study <a122000tw@gmail.com>
2021-05-15 12:07:10 +0800

    score

--- app_sqlite/src/main/java/com/applab/app_sqlite/MainActivity.kt
+++ app_sqlite/src/main/java/com/applab/app_sqlite/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.applab.app_sqlite
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.applab.app_sqlite.db.DBHelper
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val dbHelper = DBHelper(this)
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+
+        title = dbHelper.databaseName
+        // 按下新增
+        btn_create.setOnClickListener {
+            create()
+        }
     }
+    // 新增紀錄
+    fun create() {
+        val name = et_name.text.toString()
+        val score = et_score.text.toString()
+        dbHelper.createStudent(name, score.toInt())
+        cleanFields()
+    }
+    // 清除欄位資料
+    fun cleanFields() {
+        et_id.setText("")
+        et_name.setText("")
+        et_score.setText("")
+    }
+
 }
--- app_sqlite/src/main/java/com/applab/app_sqlite/db/DBHelper.kt
+++ app_sqlite/src/main/java/com/applab/app_sqlite/db/DBHelper.kt
@@ -1,7 +1,14 @@
 package com.applab.app_sqlite.db
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import java.util.*
+
 // 撰寫與資料庫建立，升級，銷毀，與CRUD相關程序
-class DBHelper {
+class DBHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     // SingleTon (companion object相當於 Java 的 static inner class 靜態內部類別)
     companion object {
         val DATABASE_VERSION = 1 // 資料庫版本
@@ -18,4 +25,30 @@ class DBHelper {
         val SQL_DELETE_STUDENT = "DROP TABLE IF EXISTS Student"
 
     }
+    // 預設資料表的建立
+    override fun onCreate(db: SQLiteDatabase?) {
+        // db 指的是 MyDB.db 資料庫容器
+        db?.execSQL(SQL_CREATE_STUDENT)
+    }
+    // 資料庫升級
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(SQL_DELETE_STUDENT)
+        onCreate(db)
+    }
+    // 新增資料
+    fun createStudent(name: String, score: Int) {
+        // 取得資料庫
+        val db = writableDatabase
+        // 準備要新增的紀錄
+        val values = ContentValues()
+        values.put("name", name)
+        values.put("score", score)
+        values.put("ct", Date().time)
+        // 新增到資料庫
+        // nullColumnHack 是指若欄位是空白要放的內容為何?
+        // action 異動筆數(此次影響資料表的紀錄數量)
+        val action = db.insert("Student", null, values)
+        Log.d("DB", "createStudent: action=" + action)
+    }
+
 }
